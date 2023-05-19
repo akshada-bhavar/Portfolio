761c0f8e23209bb19b2534d76cf3d4923edca357
Akshu <akshada.bhavar10@gmail.com>
2023-05-19 18:19:55 +0530

    Add files

---
+++ main.js
@@ -0,0 +1,32 @@
+(function ($) {
+    
+
+    
+
+     // Scroll to Bottom
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-to-bottom').fadeOut('slow');
+        } else {
+            $('.scroll-to-bottom').fadeIn('slow');
+        }
+    });
+
+
+     // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    
+    
+})(jQuery);
+
